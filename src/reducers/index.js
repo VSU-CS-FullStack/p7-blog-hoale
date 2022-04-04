0993424a0037a8099f202db242516a23da19c658
hnqle3 <hoa.nq.le3@gmail.com>
2022-04-04 00:52:53 -0400

    created posts in the redux store

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,7 @@
 import { combineReducers } from "redux";
-const rootReducer =combineReducers({
-})
+import PostsReducer from "./reducer_posts";
+const rootReducer = combineReducers({
+    posts: PostsReducer
+});
 
 export default rootReducer
